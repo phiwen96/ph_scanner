6376c40c928e50a66c0e6786dc1ae87607c69b68
Philip Wenkel <38218064+phiwen96@users.noreply.github.com>
2021-05-19 20:24:32 +0200

    a

--- include/ph_scanner/scanner.hpp
+++ include/ph_scanner/scanner.hpp
@@ -1,3 +1,162 @@
 #pragma once
+#include <ph_scanner/token.hpp>
 
 using namespace std;
+
+#define IS_DIGIT(x) (x >= '0' and x <= '9')
+
+
+
+struct scanner
+{
+    string m_source;
+    vector <token> m_tokens;
+    
+    int m_start {0};
+    int m_current {0};
+    int m_line {1};
+    
+    
+    
+    vector <token> scan_tokens ()
+    {
+        while (not is_at_end ())
+        {
+            m_start = m_current;
+            
+            
+        }
+    }
+    
+    
+    
+private:
+    void scan_token ()
+    {
+        char c = advance ();
+        
+        if (c == '-')
+        {
+            //            value.push_back ('-');
+            add_token (token::type::minus);
+            //            goto minus;
+            
+        } else if (c == '+')
+        {
+            //            cout << "ADD" << endl;
+            add_token (token::type::star);
+            
+        } else if (c == '(')
+        {
+            add_token (token::type::left_paranthesis);
+            
+        } else if (c == ')')
+        {
+            add_token (token::type::right_paranthesis);
+            
+        } else if (c == '/')
+        {
+            if (match ('/'))
+            {
+                while (peek () != '\n')
+                {
+                    advance ();
+                }
+                add_token (token::type::comment);
+                
+            } else
+            {
+                add_token (token::type::slash);
+            }
+            
+        } else if (c == '*')
+        {
+            add_token (token::type::star);
+            
+        } else if (c == ' ')
+        {
+            add_token (token::type::space);
+            
+        } else if (c == '.')
+        {
+            add_token (token::type::dot);
+            
+        } else if (c == ',')
+        {
+            add_token (token::type::comma);
+            
+        } else if (c == ';')
+        {
+            add_token (token::type::semicolon);
+            
+        } else if (c == ':')
+        {
+            add_token (token::type::colon);
+            
+        }  else if (c == '\n')
+        {
+            add_token (token::type::newline);
+            ++ m_line;
+            
+        } else if (c == '\t')
+        {
+            add_token (token::type::tab);
+            
+        } else if (IS_DIGIT (c))
+        {
+            number ();
+        } else if (c == '!')
+        {
+            add_token (match ('=') ? token::type::bang_equal : token::type::bang);
+        }
+    }
+    auto advance () -> char
+    {
+        return m_source [m_current++];
+    }
+    /**
+     It’s like a conditional advance(). We only consume the current character if it’s what we’re looking for.
+     */
+    auto match (char expected) -> bool
+    {
+        if (is_at_end ()) return false;
+        if (m_source [m_current] != expected) return false;
+        
+        m_current++;
+        return true;
+    }
+    void add_token (token::type type)
+    {
+        m_tokens.push_back (token {
+            type,
+            string {m_source.begin (), m_source.begin () + m_current},
+            m_line
+        });
+    }
+    auto is_at_end () -> bool
+    {
+        return m_current >= m_source.length ();
+    }
+    auto peek() -> char
+    {
+        if (is_at_end ()) return '\0';
+        return m_source [m_current];
+    }
+    auto number () -> void
+    {
+        while (IS_DIGIT (peek ()))
+        {
+            advance ();
+        }
+        
+        if (peek () == '.')
+        {
+            while (IS_DIGIT (peek ()))
+            {
+                advance ();
+            }
+        }
+        
+        add_token (token::type::number);
+    }
+};
---
+++ include/ph_scanner/token.hpp
@@ -0,0 +1,113 @@
+#pragma once
+
+using namespace std;
+
+/**
+ // Single-character tokens.
+   LEFT_PAREN, RIGHT_PAREN, LEFT_BRACE, RIGHT_BRACE,
+   COMMA, DOT, MINUS, PLUS, SEMICOLON, SLASH, STAR,
+
+   // One or two character tokens.
+   BANG, BANG_EQUAL,
+   EQUAL, EQUAL_EQUAL,
+   GREATER, GREATER_EQUAL,
+   LESS, LESS_EQUAL,
+
+   // Literals.
+   IDENTIFIER, STRING, NUMBER,
+
+   // Keywords.
+   AND, CLASS, ELSE, FALSE, FUN, FOR, IF, NIL, OR,
+   PRINT, RETURN, SUPER, THIS, TRUE, VAR, WHILE,
+
+   EOF
+ */
+
+#define TOKENS \
+X (bang) \
+X (bang_equal) \
+X (equal_equal) \
+X (equal) \
+X (less) \
+X (greater) \
+X (left_paranthesis) \
+X (right_paranthesis) \
+X (plus) \
+X (minus) \
+X (star) \
+X (division) \
+X (number) \
+X (variable) \
+X (left_curly_bracket) \
+X (right_curly_bracket) \
+X (left_bracket) \
+X (right_bracket) \
+X (dot) \
+X (semicolon) \
+X (space) \
+X (comma) \
+X (tab) \
+X (newline) \
+X (colon) \
+X (unknown) \
+X (factor) \
+X (term) \
+X (expression) \
+X (slash) \
+X (comment)
+
+
+
+
+
+
+struct token
+{
+    enum struct type
+    {
+#define X(a) \
+a,
+        TOKENS
+#undef X
+        
+    };
+    
+//    auto operator== (token const& other) -> bool
+//    {
+//        return m_type == other.m_type and m_lexeme == other.m_lexeme;
+//    }
+//
+//    auto operator== (type const& t) -> bool
+//    {
+//        return m_type == t;
+//    }
+    
+    type m_type;
+    string m_lexeme;
+    int m_line;
+    
+    
+};
+
+
+
+
+
+
+inline ostream& operator<< (ostream& os, token const& t)
+{
+    switch (t.m_type)
+    {
+#define X(x) \
+case token::type::x: \
+os << "<" << #x << ">"; \
+break;
+            
+            TOKENS
+#undef X
+    }
+    
+    return os;
+}
+
+#undef TOKENS
